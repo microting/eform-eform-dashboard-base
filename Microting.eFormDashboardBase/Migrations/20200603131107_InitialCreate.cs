a59036b781bea2de2e09aed61e5798e1b7b11c13
René Schultz Madsen <rm@microting.com>
2021-07-05 16:19:02 +0200

    Fixing migration files.

--- Microting.eFormDashboardBase/Migrations/20200603131107_InitialCreate.cs
+++ Microting.eFormDashboardBase/Migrations/20200603131107_InitialCreate.cs
@@ -29,7 +29,7 @@ namespace Microting.eFormDashboardBase.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             string autoIDGenStrategy = "SqlServer:ValueGenerationStrategy";
-            object autoIDGenStrategyValue= SqlServerValueGenerationStrategy.IdentityColumn;
+            object autoIDGenStrategyValue= MySqlValueGenerationStrategy.IdentityColumn;
 
             // Setup for MySQL Provider
             if (migrationBuilder.ActiveProvider == "Pomelo.EntityFrameworkCore.MySql")
